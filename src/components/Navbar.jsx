01e549abb5d2574a4223fc6ef9684cae3f5bea52
liviaalcantaraf <liviaalcantaraf@gmail.com>
2022-05-13 16:54:07 +0100

    inicial navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import styled from "styled-components";
+
+let Container = styled.div`
+  height: 30px;
+`;
+
+let Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+let Left = styled.div`
+  width: 60%;
+`;
+let Logo = styled.h1`
+  font-weight: bold;
+  color: #363062;
+  text-decoration: underline #e9d5da;
+`;
+let Right = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+let Menu = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+let MenuItem = styled.li`
+  display: inline-block;
+  padding-bottom: 0.25rem;
+  position: relative;
+  margin-right: 80px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #827397;
+  cursor: pointer;
+
+  ::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 0;
+    height: 2px;
+    background-color: #e9d5da;
+    transition: width 0.25s ease-out;
+  }
+  :hover::before {
+    width: 100%;
+  }
+`;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Container>
+        <Wrapper>
+          <Left>
+            <Logo>Alec P.</Logo>
+          </Left>
+          <Right>
+            <Menu>
+              <MenuItem>Home</MenuItem>
+              <MenuItem>Portfolio</MenuItem>
+              <MenuItem>About Me</MenuItem>
+              <MenuItem>Contact</MenuItem>
+            </Menu>
+          </Right>
+        </Wrapper>
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
